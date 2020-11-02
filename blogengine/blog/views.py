f57a6e0c512bc2a387b84e2fd902c9f5fce8fbf8
GooseArkasha <popoval010900@gmail.com>
2020-11-02 23:48:58 +0300

    first commit

---
+++ blogengine/blog/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
+from django.views.generic import View
+
+
+from .models import Post, Tag
+from .utils import *
+from .forms import TagForm, PostForm
+
+
+def posts_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+def tags_list (request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags':tags})
+
+
+class PostDetail(objectsDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+
+class TagDetail(objectsDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+
+class PostCreate(ObjectCreateMixin, View):
+    form_model = PostForm
+    template = 'blog/post_create.html'
+
+class TagCreate(ObjectCreateMixin, View):
+    form_model = TagForm
+    template = 'blog/tag_create.html'
+
+class TagUpdate(ObjectUpdateMixin, View):
+    model = Tag
+    model_form = TagForm
+    template = 'blog/tag_update_form.html'
+
+class PostUpdate(ObjectUpdateMixin, View):
+    model = Post
+    model_form = PostForm
+    template = 'blog/post_update_form.html'
