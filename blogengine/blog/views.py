fb7bc003ed63cfef60bab6114669fd44944629fe
GooseArkasha <popoval010900@gmail.com>
2020-11-03 14:08:18 +0300

    second commit

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
+from django.urls import reverse
 from django.views.generic import View
 
 
 from .models import Post, Tag
 from .utils import *
 from .forms import TagForm, PostForm
+
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 def posts_list(request):
@@ -26,20 +29,36 @@ class TagDetail(objectsDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
 
-class PostCreate(ObjectCreateMixin, View):
+class PostCreate(LoginRequiredMixin, ObjectCreateMixin, View):
     form_model = PostForm
     template = 'blog/post_create.html'
+    raise_exeption = True
 
-class TagCreate(ObjectCreateMixin, View):
+class TagCreate(LoginRequiredMixin, ObjectCreateMixin, View):
     form_model = TagForm
     template = 'blog/tag_create.html'
+    raise_exeption = True
 
-class TagUpdate(ObjectUpdateMixin, View):
+class TagUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
     model = Tag
     model_form = TagForm
     template = 'blog/tag_update_form.html'
+    raise_exeption = True
 
-class PostUpdate(ObjectUpdateMixin, View):
+class PostUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
     model = Post
     model_form = PostForm
     template = 'blog/post_update_form.html'
+    raise_exeption = True
+
+class TagDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
+    model = Tag
+    template = 'blog/tag_delete_form.html'
+    redirect_url = 'tags_list_url'
+    raise_exeption = True
+
+class PostDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
+    model = Post
+    template = 'blog/post_delete_form.html'
+    redirect_url = 'posts_lists_url'
+    raise_exeption = True
--- blogengine/blogengine/views.py
+++ blogengine/blogengine/views.py
@@ -1,1 +1,4 @@
+from django.shortcuts import redirect
 
+def redirect_blog(request):
+    return redirect('posts_lists_url', permanent=True)
